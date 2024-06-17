a21b8893c5c00b51d8b03bfcce5207b7ab89d991
tothanhtin123 <tothanhtin123@gmail.com>
2024-06-17 22:00:09 +0700

    feat: chapter 12

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({children}:{children: React.ReactNode}) => {
         <div className="w-full flex-none md:w-64">
             <SideNav/>
         </div>
-        <div className="flex-row p-6 md:overflow-y-auto md:p-12">{children}</div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
 }
 export default Layout;
