1bd3130c34f2b5bc3cf086235db2c4bcf388246f
tothanhtin123 <tothanhtin123@gmail.com>
2024-06-22 14:27:37 +0700

    feat: chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,8 @@ import {z} from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -71,7 +72,7 @@ export async function createInvoice(prevState: State, formData: FormData){
 
 export async function updateInvoice(id: string, prevState: State, formData: FormData) {
    // Validate form using Zod
-    const validatedFields = CreateInvoice.safeParse({
+    const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
@@ -110,4 +111,23 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
       return { message: 'Database Error: Failed to Delete Invoice.' };
     }
     
+  }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
   }
